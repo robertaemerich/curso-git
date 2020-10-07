07df4aee063508ae6ac347d2327102e5b4eb19c6
Roberta Emerich <robertaemiranda@gmail.com>
2020-10-07 18:45:54 -0300

    inicio do projeto

---
+++ curso-git/curso-git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+        }
+    }
+}
